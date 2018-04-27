a8875c455a1b801419fa03ef045c1f9abbc4b1ff
mshuma <mastershumakov@gmail.com>
2018-04-27 17:37:41 +0300

    First commit, resources added

---
+++ window.rb
@@ -0,0 +1,56 @@
+require 'gosu'
+
+
+class Window < Gosu::Window
+  def initialize
+    super 800, 800, fullscreen = false
+    self.caption = "Racing game"
+    @track_image = Gosu::Image.new('res/track.jpg')
+    @car_image = Gosu::Image.new('res/car.png')
+    @image = Gosu::Image.new('res/image.png')
+    @angle = 0
+    @x = 320
+    @y = 240
+    @active = false
+  end
+
+  def draw
+    @track_image.draw(800, 0, 0, -1)
+    @image.draw_rot(@x, 300, 1, @angle)
+
+  end
+
+  # def rotate
+  #   @image.rotate
+  # end
+
+  def update
+    if @active
+      # @x += 3
+      @angle += 3
+    else
+      @angle -= 3
+    end
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_UP
+      @active = if @active
+                  false
+                else
+                  true
+                end
+    end
+    if id == Gosu::KB_ESCAPE
+      self.close
+    end
+  end
+
+  def circle_moving(x_0, y_0, angle)
+
+  end
+end
+
+wheel = Wheel.new
+wheel.show
+# wheel.draw
