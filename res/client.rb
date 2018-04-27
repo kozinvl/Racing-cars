e9da6d3915aa2b6ca4380eadb23ac2bf7e2e5a2b
vkozin <vladiksystem@gmail.com>
2018-04-27 17:30:02 +0300

    Base commit to repository

---
+++ res/client.rb
@@ -0,0 +1,9 @@
+require "thread"
+require "socket"
+
+class Client
+
+  PORT = 12000
+  HOST = "xx.xx.xx.xx"
+
+end
---
+++ res/client_server.rb
@@ -0,0 +1,10 @@
+require "socket"
+require "thread"
+
+
+class ClientServer
+  PORT = 12000
+  HOST = "10.129.201.49"
+
+
+end
