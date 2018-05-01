9f6cedadb3772337c58d41df4be3a3a164bc7817
nshumakov <mastershumakov@gmail.com>
2018-05-02 00:27:51 +0300

    Another game moments added

--- my_script.rb
+++ my_script.rb
@@ -17,7 +17,7 @@
 #  4) similarly, add sound effects for various events
 # Exploring this game's code and Gosu:
 #  5) make the player wider, so he doesn't fall off edges as easily
-#  6) add background music (check if playing in Window#update to implement 
+#  6) add background music (check if playing in Window#update to implement
 #     looping)
 #  7) implement parallax scrolling for the star background!
 # Getting tricky:
@@ -30,24 +30,27 @@
 require 'rubygems'
 require 'gosu'
 
-WIDTH, HEIGHT = 1000, 1000
+WIDTH, HEIGHT = 800, 800
 
+module ZOrder
+  BACKGROUND, STARS, PLAYER, UI = *0..3
+end
 module Tiles
   Grass = 0
   Earth = 1
 end
 
-class CollectibleGem
+class Tree
   attr_reader :x, :y
 
   def initialize(image, x, y)
     @image = image
     @x, @y = x, y
   end
-  
+
   def draw
     # Draw, slowly rotating
-    @image.draw_rot(@x, @y, 0, 25 * Math.sin(Gosu.milliseconds / 133.7))
+    @image.draw(@x, @y, 0)
   end
 end
 
@@ -57,75 +60,65 @@ class Player
 
   def initialize(map, x, y)
     @x, @y = x, y
+    @vel_x = @vel_y = 0.0
+    @angle = 90.0
+    @score = 0
     @dir = :left
     @vy = 0 # Vertical velocity
     @map = map
     # Load all animation frames
-    @standing, @walk1, @walk2, @jump = *Gosu::Image.load_tiles("media/cptn_ruby.png", 50, 50)
+    @image = Gosu::Image.new('res/car.png')
     # This always points to the frame that is currently drawn.
     # This is set in update, and used in draw.
-    @cur_image = @standing    
   end
-  
-  def draw
-    # Flip vertically when facing to the left.
-    if @dir == :left
-      offs_x = -25
-      factor = 1.0
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+
+  def turn_left
+    @angle -= 4.5
+  end
+
+  def turn_right
+    @angle += 4.5
+  end
+
+  def accelerate
+    @vel_x += Gosu.offset_x(@angle, 0.5)
+    @vel_y += Gosu.offset_y(@angle, 0.5)
+  end
+
+  def move
+    if condition
+      @x += @vel_x
+      @y += @vel_y
     else
-      offs_x = 25
-      factor = -1.0
+      @x -= @vel_x
+      @y -= @vel_y
     end
-    @cur_image.draw(@x + offs_x, @y - 49, 0, factor, 1.0)
+    @vel_x *= 0.9
+    @vel_y *= 0.9
   end
-  
+
+  def condition
+    if (20...780).include?(@x) and (0...800).include?(@y)
+      true
+    end
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, ZOrder::PLAYER, @angle)
+  end
+
   # Could the object be placed at x + offs_x/y + offs_y without being stuck?
   def would_fit(offs_x, offs_y)
     # Check at the center/top and center/bottom for map collisions
     not @map.solid?(@x + offs_x, @y + offs_y) and
-      not @map.solid?(@x + offs_x, @y + offs_y - 45)
+        not @map.solid?(@x + offs_x, @y + offs_y - 45)
   end
-  
-  def update(move_x)
-    # Select image depending on action
-    if move_x == 0
-      @cur_image = @standing
-    else
-      @cur_image = (Gosu.milliseconds / 175 % 2 == 0) ? @walk1 : @walk2
-    end
-    if @vy < 0
-      @cur_image = @jump
-    end
-    
-    # Directional walking, horizontal movement
-    if move_x > 0
-      @dir = :right
-      move_x.times { if would_fit(1, 0) then @x += 1 end }
-    end
-    if move_x < 0
-      @dir = :left
-      (-move_x).times { if would_fit(-1, 0) then @x -= 1 end }
-    end
 
-    # Acceleration/gravity
-    # By adding 1 each frame, and (ideally) adding vy to y, the player's
-    # jumping curve will be the parabole we want it to be.
-    @vy += 1
-    # Vertical movement
-    if @vy > 0
-      @vy.times { if would_fit(0, 1) then @y += 1 else @vy = 0 end }
-    end
-    if @vy < 0
-      (-@vy).times { if would_fit(0, -1) then @y -= 1 else @vy = 0 end }
-    end
-  end
-  
-  def try_to_jump
-    if @map.solid?(@x, @y + 1)
-      @vy = -20
-    end
-  end
-  
+
   def collect_gems(gems)
     # Same as in the tutorial game.
     gems.reject! do |c|
@@ -137,12 +130,12 @@ end
 # Map class holds and draws tiles and gems.
 class Map
   attr_reader :width, :height, :gems
-  
+
   def initialize(filename)
     # Load 60x60 tiles, 5px overlap in all four directions.
-    @tileset = Gosu::Image.load_tiles("media/tileset.png", 60, 60, :tileable => true)
+    @tileset = Gosu::Image.load_tiles("media/tileset.png", 80, 80, :tileable => true)
 
-    gem_img = Gosu::Image.new("media/gem.png")
+    gem_img = Gosu::Image.new('media/tree.png')
     @gems = []
 
     lines = File.readlines(filename).map { |line| line.chomp }
@@ -151,20 +144,20 @@ class Map
     @tiles = Array.new(@width) do |x|
       Array.new(@height) do |y|
         case lines[y][x, 1]
-        when '"'
-          Tiles::Grass
-        when '#'
-          Tiles::Earth
-        when 'x'
-          @gems.push(CollectibleGem.new(gem_img, x * 50 + 25, y * 50 + 25))
-          nil
-        else
-          nil
+          when '"'
+            Tiles::Grass
+          when '#'
+            Tiles::Earth
+          when 'x'
+            @gems.push(Tree.new(gem_img, x * 50 + 25, y * 50 + 25))
+            nil
+          else
+            nil
         end
       end
     end
   end
-  
+
   def draw
     # Very primitive drawing function:
     # Draws all the tiles, some off-screen, some on-screen.
@@ -180,7 +173,7 @@ class Map
     end
     @gems.each { |c| c.draw }
   end
-  
+
   # Solid at a given pixel position?
   def solid?(x, y)
     y < 0 || @tiles[x / 50][y / 50]
@@ -190,45 +183,55 @@ end
 class CptnRuby < (Example rescue Gosu::Window)
   def initialize
     super WIDTH, HEIGHT
-    
+
     self.caption = "Cptn. Ruby"
-    
-    @sky = Gosu::Image.new("media/space.png", :tileable => true)
+
+    @sky = Gosu::Image.new("res/track.jpg", :tileable => true)
     @map = Map.new("media/cptn_ruby_map.txt")
-    @cptn = Player.new(@map, 400, 100)
+    @player = Player.new(@map, 400, 650)
     # The scrolling position is stored as top left corner of the screen.
     @camera_x = @camera_y = 0
   end
-  
+
   def update
-    move_x = 0
-    move_x -= 5 if Gosu.button_down? Gosu::KB_LEFT
-    move_x += 5 if Gosu.button_down? Gosu::KB_RIGHT
-    @cptn.update(move_x)
-    @cptn.collect_gems(@map.gems)
+    @player.move
+    @player.turn_left if Gosu.button_down? Gosu::KB_LEFT and @player.condition
+    @player.turn_right if Gosu.button_down? Gosu::KB_RIGHT and @player.condition
+    @player.accelerate if Gosu.button_down? Gosu::KB_UP and @player.condition
+
+
+    # move_x = 0
+    # move_x -= 5 if Gosu.button_down? Gosu::KB_LEFT
+    # move_x += 5 if Gosu.button_down? Gosu::KB_RIGHT
+    # move_y = 0
+    # move_y -= 5 if Gosu.button_down? Gosu::KB_UP
+    # move_y += 5 if Gosu.button_down? Gosu::KB_DOWN
+    # @player.update(move_x, move_y)
+    @player.collect_gems(@map.gems)
     # Scrolling follows player
-    @camera_x = [[@cptn.x - WIDTH / 2, 0].max, @map.width * 50 - WIDTH].min
-    @camera_y = [[@cptn.y - HEIGHT / 2, 0].max, @map.height * 50 - HEIGHT].min
+    @camera_x = [[@player.x - WIDTH / 2, 0].max, @map.width * 50 - WIDTH].min
+    @camera_y = [[@player.y - HEIGHT / 2, 0].max, @map.height * 50 - HEIGHT].min
   end
-  
+
   def draw
     @sky.draw 0, 0, 0
-    Gosu.translate(-@camera_x, -@camera_y) do
+    # Gosu.translate(-@camera_x, -@camera_y) do
       @map.draw
-      @cptn.draw
-    end
+      @player.draw
+    # end
   end
-  
+
   def button_down(id)
     case id
-    when Gosu::KB_UP
-      @cptn.try_to_jump
-    when Gosu::KB_ESCAPE
-      close
-    else
-      super
+      # when Gosu::KB_UP
+      #   @cptn.try_to_jump
+      when Gosu::KB_ESCAPE
+        close
+      else
+        super
     end
   end
+
 end
 
 CptnRuby.new.show if __FILE__ == $0
